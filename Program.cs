4f41244539e6133c4ead5d1117e333975a14c454
grimroot23 <nick.dupe21860@gmail.com>
2022-10-13 19:13:46 -0600

    try again

--- Program.cs
+++ Program.cs
@@ -2,6 +2,37 @@
 
 namespace CSE210_Hilo 
 {
-    
+    class init_card_score
+    {
+        private int card = 0;
+        public int Card
+        {
+            get{return card;}
+            set{card=value;}
+        }
+        //---------------------------------------
+        private int lastcard = 0;
+        public int Lastcard
+        {
+            get{return lastcard;}
+            set{lastcard=value;}
+        }
+        //---------------------------------------
+        int score = 300;
+    }
+
+    class begin_game
+    {
+        
+        static void Main(string[] args)
+        {
+            int new_card = new Random().Next(1, 13);
+
+            init_card_score draw_card = new init_card_score();
+            draw_card.Card = new_card;
+            draw_card.Lastcard = new_card;
+        }
+    }
+
     
 }
